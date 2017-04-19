45850482c32c55811b0a2bb722e782f587d0ba9d
wingrider <sreejan.mj@live.com>
2017-04-18 22:17:19 -0500

    added comments

--- comment.py
+++ comment.py
@@ -1,5 +1,6 @@
 # learn python again
 print "Start again"
 
+#multi-line comment
 """Trying multi-line
 comment in python"""
--- ex9.py
+++ ex9.py
@@ -1,18 +1,20 @@
+#format modifier
 weekFormatter = "%s " * 7
 monthFormatter = "%s\n" * 12
 
-print "Here are the days :" + weekFormatter % ('Sun', 'Mon', 'Tue', 'Wed', 
+#using format modifier
+print "Here are the days :" + weekFormatter % ('Sun', 'Mon', 'Tue', 'Wed',
 	'Thu', 'Fri', 'Sat')
-print "Here are the months : " + monthFormatter % ('Jan', 'Feb', 'Mar', 
+print "Here are the months : " + monthFormatter % ('Jan', 'Feb', 'Mar',
 	'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec')
-	
+
 # Another way
 days = "Mon Tue Wed Thu Fri Sat Sun"
 months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
-
 print "Here are the days:" , days
 print "Here are the months:" , months
- 
+
+#print multi-line string
 print """
  This cane be a string with any lines.
  By any line I meant absolutely any number of lines.
